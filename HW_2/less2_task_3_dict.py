2c4ec27bd2b3f34d87911345a5ea2f280400ca23
Evgeniya Ivanova <ivaernik@gmail.com>
2021-12-15 21:46:13 +0300

    Lesson 2 tasks 1-6 added

---
+++ HW_2/less2_task_3_dict.py
@@ -0,0 +1,6 @@
+my_dict_1 = {1: 'winter', 2: 'winter', 3: 'spring', 4: 'spring', 5: 'spring',
+             6: 'summer', 7: 'summer', 8: 'summer', 9: 'fall',
+             10: 'fall', 11: 'fall', 12: 'winter'}
+
+number_mth = int(input("Enter month's number  1 - 12:  "))
+print(my_dict_1[number_mth])
